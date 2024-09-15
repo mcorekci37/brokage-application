c29117e25169d02253636a7f496d6c0be7bf2b16
mcorekci <mustafa.corekci@dzsi.com>
2024-09-15 23:47:13 +0300

    first commit

---
+++ src/test/java/com/emce/brokage/BrokageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emce.brokage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
